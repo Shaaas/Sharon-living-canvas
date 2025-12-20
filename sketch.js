aba18dfa5f562570fdb165d8ff9a05e8c111fcd6
Sharon Mutanu <mutanusharon0@gmail.com>
2025-12-20 20:51:07 +0300

    Add animated dots to canvas in sketch.js

---
+++ sketch.js
@@ -0,0 +1,36 @@
+let dots = [];
+
+function setup() {
+  const canvas = createCanvas(windowWidth, windowHeight);
+  canvas.parent(document.body);
+
+  for (let i = 0; i < 120; i++) {
+    dots.push({
+      x: random(width),
+      y: random(height),
+      r: random(1, 3),
+      dx: random(-0.3, 0.3),
+      dy: random(-0.3, 0.3)
+    });
+  }
+}
+
+function draw() {
+  clear();
+
+  dots.forEach(dot => {
+    dot.x += dot.dx;
+    dot.y += dot.dy;
+
+    if (dot.x < 0 || dot.x > width) dot.dx *= -1;
+    if (dot.y < 0 || dot.y > height) dot.dy *= -1;
+
+    noStroke();
+    fill(255, 255, 255, 120);
+    circle(dot.x, dot.y, dot.r);
+  });
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
